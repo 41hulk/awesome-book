4133b884e7e6cb8cc550a09bf4609d6f96c17837
Ntare Guy <gntare2@gmail.com>
2022-02-08 16:35:22 +0200

    Add linters

--- final.js
+++ final.js
@@ -1,69 +1,69 @@
 function Book(title, author, id) {
-	this.title = title;
-	this.author = author;
-	this.id = id;
+  this.title = title;
+  this.author = author;
+  this.id = id;
 }
 
 let myBooks = [];
-const container = document.querySelector(".book-container");
+const container = document.querySelector('.book-container');
 
 function addBook(book) {
-	const bookDiv = document.createElement("div");
-	const h2 = document.createElement("h2");
-	const h3 = document.createElement("h3");
-	const removeBtn = document.createElement("button");
-	removeBtn.classList.add("delete");
-	h2.textContent = book.title;
-	h3.textContent = book.author;
-	removeBtn.textContent = "Remove";
-	removeBtn.setAttribute("id", book.id);
-	bookDiv.appendChild(h2);
-	bookDiv.appendChild(h3);
-	bookDiv.appendChild(removeBtn);
-	container.appendChild(bookDiv);
-	const bookJson = JSON.stringify(myBooks);
-	localStorage.setItem("books", bookJson);
+  const bookDiv = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const h3 = document.createElement('h3');
+  const removeBtn = document.createElement('button');
+  removeBtn.classList.add('delete');
+  h2.textContent = book.title;
+  h3.textContent = book.author;
+  removeBtn.textContent = 'Remove';
+  removeBtn.setAttribute('id', book.id);
+  bookDiv.appendChild(h2);
+  bookDiv.appendChild(h3);
+  bookDiv.appendChild(removeBtn);
+  container.appendChild(bookDiv);
+  const bookJson = JSON.stringify(myBooks);
+  localStorage.setItem('books', bookJson);
 }
 
 function displayBooks() {
-	myBooks.forEach((element) => {
-		addBook(element);
-	});
+  myBooks.forEach((element) => {
+    addBook(element);
+  });
 }
 
 function clearFields() {
-	document.querySelector("#title").value = "";
-	document.querySelector("#author").value = "";
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
 }
 
 function deleteBook(bookId) {
-	myBooks = myBooks.filter((book) => book.id !== bookId);
+  myBooks = myBooks.filter((book) => book.id !== bookId);
 }
 
-const form = document.querySelector("#form");
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const title = document.querySelector("#title").value;
-	const author = document.querySelector("#author").value;
-	const id = Math.random()
-		.toString(36)
-		.replace(/[^a-z]+/g, "")
-		.substr(0, 5);
-	//Instantiate Book
-	const book = new Book(title, author, id);
-	//Display Book in UI
-	myBooks = myBooks.concat(book);
-	addBook(book);
-	console.log(myBooks);
-	//Clear Fields
-	clearFields();
+const form = document.querySelector('#form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const id = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5);
+  // Instantiate Book
+  const book = new Book(title, author, id);
+  // Display Book in UI
+  myBooks = myBooks.concat(book);
+  addBook(book);
+  console.log(myBooks);
+  // Clear Fields
+  clearFields();
 });
 
-window.addEventListener("DOMContentLoaded", displayBooks);
+window.addEventListener('DOMContentLoaded', displayBooks);
 
-container.addEventListener("click", (e) => {
-	if (e.target.classList.contains("delete")) {
-		e.target.parentElement.remove();
-		deleteBook(e.target.id);
-	}
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+    deleteBook(e.target.id);
+  }
 });
